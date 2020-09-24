a9c1bf271c3a98a06a95c0d96b3894bb92d3e89f
tanviralam1998 <tanvir.ctg1998@gmail.com>
2020-09-24 10:34:33 +0600

    singup.php

--- class-21/superglobals.php
+++ class-21/superglobals.php
@@ -3,13 +3,20 @@
     //$_SERVER
     //$_POST
     //$_GET
-    $x=10;
-    $y=20;
 
-    function multipication(){
-        $mul=$GLOBALS['x'] * $GLOBALS['y'];
-        echo $mul;
-    }
-    multipication();
+    //$GLOBALS
+    // $x=10;
+    // $y=20;
 
+    // function multipication(){
+    //     $mul=$GLOBALS['x'] * $GLOBALS['y'];
+    //     echo $mul;
+    // }
+    // multipication();
+
+    // echo $_SERVER['PHP_SELF'];
+    // echo $_SERVER['SERVER_NAME'];
+    // echo $_SERVER['HTTP_HOST'];
+    echo $_SERVER['REMOTE_HOST'];
+    
 ?>
